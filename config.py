e990a8b73187a92b78b58e15e7c84bf03fb4fd5d
Karan Gobade <ssslandge12@gmail.com>
2026-06-15 18:14:54 +0530

    db

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/user"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:nFxXfqWlZImQmWhadDNwCOogKqjvTnle@thomas.proxy.rlwy.net:32210/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
